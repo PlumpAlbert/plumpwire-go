8f5bbc147044f9f44ecc373e06aaaf36409c877d
Matthew Fedin <plumpalbert@gmail.com>
2024-11-16 18:55:21 +0300

    create global invoice manager

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"os/signal"
 
 	"github.com/go-telegram/bot"
+	"plumpalbert.xyz/plumpwire/invoice"
 	"plumpalbert.xyz/plumpwire/models"
 	"plumpalbert.xyz/plumpwire/wgez"
 )
 
 var config models.AppConfig
 var wg wgez.WGEasy
+var im *invoice.InvoiceManager
 
 func LoadConfig() error {
 	config.WG_HOST = os.Getenv("WG_HOST")
@@ -25,6 +27,11 @@ func LoadConfig() error {
 		return errors.New("please provide TELEGRAM_TOKEN environment variable")
 	}
 
+	config.INVOICE_HOST = os.Getenv("INVOICE_HOST")
+	if config.INVOICE_HOST == "" {
+		return errors.New("please provide INVOICE_HOST environment variable")
+	}
+
 	return nil
 }
 
@@ -35,6 +42,11 @@ func main() {
 	}
 
 	wg = wgez.New(config.WG_HOST)
+	im, err = invoice.New(config.INVOICE_HOST, os.Getenv("INVOICE_API_KEY"))
+
+	if err != nil {
+		panic(err)
+	}
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
