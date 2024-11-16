79c24d6703c3288a56b98bda58e748f5235eca93
Matthew Fedin <plumpalbert@gmail.com>
2024-11-16 14:06:37 +0300

    expose method to get new invoice manager

--- invoice/main.go
+++ invoice/main.go
@@ -1,6 +1,9 @@
 package invoice
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"plumpalbert.xyz/plumpwire/invoice/models"
 )
 
@@ -8,4 +11,29 @@ type Invoice struct {
 	endpoint string
 
 	Clients []models.Client
+}
+
+// Generate new Invoice object
+func New(host string) (*Invoice, error) {
+	invoice := Invoice{
+		endpoint: host + "/api/v1",
+		Clients:  []models.Client{},
+	}
+
+	err := invoice.GetClients()
+	if err != nil {
+		return nil, err
+	}
+
+	return &invoice, nil
+}
+
+// Get list of clients
+func (inv Invoice) GetClients() error {
+	res, err := http.Get(inv.endpoint + "/clients?status=active")
+	if err != nil {
+		return err
+	}
+
+	return json.NewDecoder(res.Body).Decode(&inv.Clients)
 }
