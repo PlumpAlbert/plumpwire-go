1bb4175a4fa2318e9ce3405ce038513c3f1165f9
Matthew Fedin <plumpalbert@gmail.com>
2024-11-16 13:55:44 +0300

    added invoice type

--- invoice/models/invoice.go
+++ invoice/models/invoice.go
@@ -7,3 +7,26 @@ type Client struct {
 	PaidToDate     float64 `json:"paid_to_date,omitempty"`
 	PaymentBalance float64 `json:"payment_balance,omitempty"`
 }
+
+type InvoiceStatus string
+
+const (
+	Draft     InvoiceStatus = "1"
+	Sent      InvoiceStatus = "2"
+	Partial   InvoiceStatus = "3"
+	Paid      InvoiceStatus = "4"
+	Cancelled InvoiceStatus = "5"
+	Reversed  InvoiceStatus = "6"
+	Overdue   InvoiceStatus = "-1"
+	Unpaid    InvoiceStatus = "-2"
+)
+
+type Invoice struct {
+	ID       string        `json:"id,omitempty"`
+	ClientID string        `json:"client_id,omitempty"`
+	Amount   float64       `json:"amount,omitempty"`
+	Balance  float64       `json:"balance,omitempty"`
+	StatusID InvoiceStatus `json:"status_id,omitempty"`
+	Date     string        `json:"date,omitempty"`
+	DueDate  string        `json:"due_date,omitempty"`
+}
